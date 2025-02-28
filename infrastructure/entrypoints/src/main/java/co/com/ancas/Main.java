94507181b3b2b1b60b92589003fd26b138bf3f10
ancas19 <andres.cmonsalve@gmail.com>
2025-02-27 22:55:54 -0500

    Feat: Create new environment variables

--- infrastructure/entrypoints/src/main/java/co/com/ancas/Main.java
+++ infrastructure/entrypoints/src/main/java/co/com/ancas/Main.java
@@ -3,9 +3,11 @@ package co.com.ancas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
+@EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan("co.com.ancas.*")
 public class Main {
