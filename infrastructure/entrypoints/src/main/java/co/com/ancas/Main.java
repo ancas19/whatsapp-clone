5a61187f2b2ba31a1fb94ef40fe618660a076169
ancas19 <andres.cmonsalve@gmail.com>
2025-03-01 16:11:52 -0500

    Feat: Add support to swagger

--- infrastructure/entrypoints/src/main/java/co/com/ancas/Main.java
+++ infrastructure/entrypoints/src/main/java/co/com/ancas/Main.java
@@ -1,5 +1,10 @@
 package co.com.ancas;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.OAuthFlow;
+import io.swagger.v3.oas.annotations.security.OAuthFlows;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
@@ -10,6 +15,19 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableJpaAuditing
 @SpringBootApplication
 @ComponentScan("co.com.ancas.*")
+@SecurityScheme(
+        name = "keycloak",
+        type = SecuritySchemeType.OAUTH2,
+        bearerFormat = "JWT",
+        scheme = "bearer",
+        in = SecuritySchemeIn.HEADER,
+        flows = @OAuthFlows(
+                password = @OAuthFlow(
+                        authorizationUrl = "http://localhost:9090/realms/whatsapp-clone/protocol/openid-connect/auth",
+                        tokenUrl = "http://localhost:9090/realms/whatsapp-clone/protocol/openid-connect/token"
+                )
+        )
+)
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
