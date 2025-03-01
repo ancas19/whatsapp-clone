860fe45082a461d0be7010ccf5149bc6012b0abb
ancas19 <andres.cmonsalve@gmail.com>
2025-02-28 22:01:04 -0500

    Feat: Create classes to use in business logic

---
+++ domain/models/src/main/java/co/com/ancas/models/domain/Messages.java
@@ -0,0 +1,20 @@
+package co.com.ancas.models.domain;
+
+import co.com.ancas.models.enums.MessageState;
+import co.com.ancas.models.enums.MessageType;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Messages {
+    private Long id;
+    private String content;
+    private MessageState state;
+    private MessageType type;
+    private String senderId;
+    private String recipientId;
+    private Chat chat;
+}
---
+++ domain/models/src/main/java/co/com/ancas/models/domain/Users.java
@@ -0,0 +1,22 @@
+package co.com.ancas.models.domain;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class Users {
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private LocalDateTime lastSeen;
+    private List<Chat> chatsAsSender;
+    private List<Chat> chatsAsRecipient;
+    boolean online;
+}
